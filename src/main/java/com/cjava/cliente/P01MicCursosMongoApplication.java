e2389133d100152434c3184ef8335ead61bb24e4
Geraldine López <geraldinelopez@MacBook-Pro-de-Geraldine-2.local>
2024-08-22 05:03:12 +0200

    first commit

---
+++ src/main/java/com/cjava/cliente/P01MicCursosMongoApplication.java
@@ -0,0 +1,13 @@
+package com.cjava.cliente;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class P01MicCursosMongoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(P01MicCursosMongoApplication.class, args);
+	}
+
+}
